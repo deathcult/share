5e1958169542085d926ec70dd82b39c4f7663713
deathcult <deathcult@163.com>
2013-10-12 11:12:58 +0800

    add new

---
+++ 网站优化方案.mm
@@ -0,0 +1,221 @@
+<map version="0.9.0">
+<!-- Copyright deathcult@163.com -->
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1377601707388" ID="ID_1562971621" MODIFIED="1381547068726" TEXT="&#x7f51;&#x7ad9;&#x4f18;&#x5316;&#x65b9;&#x6848;">
+<font BOLD="true" NAME="&#x9ed1;&#x4f53;" SIZE="15"/>
+<node CREATED="1377742657077" ID="ID_690042088" MODIFIED="1378799616350" POSITION="right" TEXT="&#x7f51;&#x7edc;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377742504272" ID="ID_726571688" MODIFIED="1378799616350" TEXT="IDC">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377742516070" ID="ID_1075485352" MODIFIED="1378799616349" TEXT="50M&#x72ec;&#x4eab;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1377742576485" ID="ID_676830490" MODIFIED="1381547142167" TEXT="&#x7535;&#x4fe1;&#x901a;&#x673a;&#x623f;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1377742517943" ID="ID_1663161639" MODIFIED="1378799616347" TEXT="CDN">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377742532893" ID="ID_1431593950" MODIFIED="1381547157014" TEXT="&#x5e1d;&#x8054;CDN">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1377742549414" ID="ID_1783867089" MODIFIED="1378799616346" TEXT="&#x7f51;&#x5bbf;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377743324110" ID="ID_1792747578" MODIFIED="1378799616339" TEXT="&#x5168;&#x56fd;&#x51e0;&#x4e2a;&#x91cd;&#x8981;&#x8282;&#x70b9;&#x7684;&#x901f;&#x5ea6;&#x6d4b;&#x8bd5;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1378790922649" ID="ID_1247153708" MODIFIED="1381547083071" TEXT="&#x5f53;&#x524d;&#x53ea;&#x6709;&#x56fe;&#x7247;&#x505a;cache&#xff1a;img/img2">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+</node>
+<node CREATED="1377744013915" ID="ID_1489249220" MODIFIED="1378799616328" POSITION="right" TEXT="&#x786c;&#x4ef6;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377829947009" ID="ID_1973408797" MODIFIED="1378799616326" TEXT="&#x786c;&#x4ef6;&#x8bbe;&#x5907;&#x914d;&#x7f6e;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1377829966495" ID="ID_801613554" MODIFIED="1378799616326" TEXT="&#x7cfb;&#x7edf;&#x8d1f;&#x8f7d;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1377742663141" ID="ID_281997854" MODIFIED="1378799616326" POSITION="right" TEXT="&#x7cfb;&#x7edf;&#x67b6;&#x6784;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377742736484" ID="ID_969777158" MODIFIED="1378799616325" TEXT="&#x7f51;&#x7ad9;&#x9996;&#x9875;&#x8d1f;&#x8f7d;&#x5747;&#x8861;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377743396544" ID="ID_1902790796" MODIFIED="1381547171615" TEXT="Nginx">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1377747468803" ID="ID_1998604005" MODIFIED="1381547103791" TEXT="&#x7f51;&#x7ad9;&#x5165;&#x53e3;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1378791088702" ID="ID_814373975" MODIFIED="1378799616308" TEXT="&#x65e0;&#x8f6e;&#x8be2;&#x3002;&#x9ed8;&#x8ba4;&#x8f6c;&#x53d1;&#x5230;243">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+</node>
+<node CREATED="1378797153684" ID="ID_962956476" MODIFIED="1378799616306" TEXT="&#x5e94;&#x7528;&#x5c42;&#x8d1f;&#x8f7d;&#x5747;&#x8861;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="12"/>
+</node>
+<node CREATED="1378797169604" ID="ID_617172479" MODIFIED="1378799616305" TEXT="&#x6570;&#x636e;&#x5e93;&#x4e3b;&#x4ece;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1377743377424" ID="ID_478794565" MODIFIED="1378799616304" POSITION="right" TEXT="&#x5e94;&#x7528;&#x670d;&#x52a1;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377743472127" ID="ID_906353721" MODIFIED="1378799616304" TEXT="Apache">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1377747395891" ID="ID_52298995" MODIFIED="1378799616303" TEXT="httpd.conf&#x914d;&#x7f6e;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1378796708602" ID="ID_294705275" MODIFIED="1378799616302" TEXT="apache&#x6a21;&#x5757;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="12"/>
+</node>
+<node CREATED="1378796720263" ID="ID_57357805" MODIFIED="1378799616294" TEXT=".htaccess&#x8bbe;&#x7f6e;&#x7b56;&#x7565;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1377743490047" ID="ID_924121745" MODIFIED="1378799616294" TEXT="Memcached">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1377747456867" ID="ID_524740893" MODIFIED="1378799616292" TEXT="&#x9002;&#x7528;&#x4e8e;&#x7f51;&#x7ad9;&#x6574;&#x4f53;php&#x7a0b;&#x5e8f;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1378790990463" ID="ID_604336930" MODIFIED="1378799616292" TEXT="items&#x6570;&#x91cf;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1378790998143" ID="ID_682959139" MODIFIED="1378799616291" TEXT="&#x8fde;&#x63a5;&#x6570;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1378796750774" ID="ID_887276469" MODIFIED="1381547196194" TEXT="&#x5185;&#x5b58;&#x4f7f;&#x7528;&#x60c5;&#x51b5;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1377743495535" ID="ID_316886153" MODIFIED="1378799616273" TEXT="Redis">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377747451651" ID="ID_1541531058" MODIFIED="1378799616271" TEXT="&#x79ef;&#x5206;&#x5151;&#x6362;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1377743498046" ID="ID_1140333082" MODIFIED="1378799616270" TEXT="Sphinx">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377747434723" ID="ID_1196280698" MODIFIED="1378799616269" TEXT="&#x5546;&#x54c1;&#x67e5;&#x8be2;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1377743512687" ID="ID_1863333840" MODIFIED="1378799616269" TEXT="PHP">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377743962924" ID="ID_511031773" MODIFIED="1378799616267" TEXT="php&#x6a21;&#x5757;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1377743968204" ID="ID_190965430" MODIFIED="1378799616267" TEXT="php&#x52a0;&#x901f;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+</node>
+<node CREATED="1377742667781" ID="ID_616562422" MODIFIED="1378799616267" POSITION="right" TEXT="&#x7a0b;&#x5e8f;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377742765524" ID="ID_1782944057" MODIFIED="1381547208438" TEXT="&#x4f2a;&#x9759;&#x6001;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1377747497090" ID="ID_1534263970" MODIFIED="1378799616264" TEXT="&#x4ea7;&#x751f;&#x9759;&#x6001;&#x9875;&#x9762;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1377742772772" ID="ID_354975360" MODIFIED="1378799616263" TEXT="&#x7a0b;&#x5e8f;&#x6267;&#x884c;&#x6548;&#x7387;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1378791153310" ID="ID_1003405998" MODIFIED="1378799616261" TEXT="CodeReview/&#x4ee3;&#x7801;&#x5ba1;&#x67e5;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1378791176254" ID="ID_1566097760" MODIFIED="1378799616260" TEXT="&#x4ee3;&#x7801;&#x91cd;&#x6784;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1377742672293" ID="ID_436976872" MODIFIED="1381547319773" POSITION="right" TEXT="&#x6570;&#x636e;&#x5e93;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377742779476" ID="ID_1520492782" MODIFIED="1378799616256" TEXT="&#x6570;&#x636e;&#x5e93;&#x670d;&#x52a1;&#x5668;&#x786c;&#x4ef6;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1377742836083" ID="ID_1064673743" MODIFIED="1378799616254" TEXT="&#x8868;&#x7ed3;&#x6784;ER&#x56fe;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1377742886979" ID="ID_1435058217" MODIFIED="1378799616252" TEXT="SQL&#x8bed;&#x53e5;&#x4f18;&#x5316;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1377747536402" ID="ID_612184785" MODIFIED="1378799616250" TEXT="&#x7f13;&#x5b58;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1378791037071" ID="ID_277330289" MODIFIED="1378799616247" TEXT="slowlog&#x68c0;&#x67e5;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1381547319774" ID="ID_1511175192" MODIFIED="1381547351778" TEXT="&#x5206;&#x5e93;/&#x5206;&#x8868;"/>
+</node>
+<node CREATED="1377752691053" ID="ID_807401420" MODIFIED="1378799616236" POSITION="left" TEXT="&#x4f18;&#x5316;&#x76ee;&#x7684;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1377752697950" ID="ID_351158544" MODIFIED="1378799616236" TEXT="&#x901f;&#x5ea6;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1377752702557" ID="ID_6154840" MODIFIED="1378799616234" TEXT="&#x7a33;&#x5b9a;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1377752706573" ID="ID_173135420" MODIFIED="1378799616232" TEXT="&#x6613;&#x4e8e;&#x7ef4;&#x62a4;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1378791227374" ID="ID_1899361421" MODIFIED="1378799616230" TEXT="&#x53ef;&#x6269;&#x5c55;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1377742676053" ID="ID_1976387613" MODIFIED="1378799616224" POSITION="left" TEXT="&#x524d;&#x7aef;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377747557698" ID="ID_190131400" MODIFIED="1378799616221" TEXT="JS&#x7a0b;&#x5e8f;&#x6548;&#x7387;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1377747563043" ID="ID_1000009758" MODIFIED="1378799616220" TEXT="&#x9875;&#x9762;&#x5927;&#x5c0f;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1377747580850" ID="ID_948929041" MODIFIED="1378799616218" TEXT="&#x9875;&#x9762;&#x672c;&#x8eab;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1377747590386" ID="ID_1432039963" MODIFIED="1378799616218" TEXT="JS/CSS">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1377747573458" ID="ID_1250313303" MODIFIED="1378799616216" TEXT="&#x56fe;&#x7247;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<node CREATED="1377747605330" ID="ID_1707103931" MODIFIED="1378799616198" TEXT="CDN&#x89e3;&#x51b3;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1377747638145" ID="ID_1726264166" MODIFIED="1378799616178" TEXT="&#x5176;&#x4ed6;&#x5f15;&#x7528;&#x52a0;&#x8f7d;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1377747669169" ID="ID_757470405" MODIFIED="1378799616177" TEXT="CPS&#x4ee3;&#x7801;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1377747673841" ID="ID_1682772286" MODIFIED="1378799616176" TEXT="&#x7edf;&#x8ba1;&#x4ee3;&#x7801;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1378791260941" ID="ID_724748951" MODIFIED="1378799616175" TEXT="SEO&#x4ee3;&#x7801;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+<node CREATED="1378791270893" ID="ID_202559191" MODIFIED="1378799616164" TEXT="&#x76d1;&#x63a7;&#x4ee3;&#x7801;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+</node>
+</node>
+<node CREATED="1378358671428" ID="ID_1751424550" MODIFIED="1378799616122" TEXT="&#x53ef;&#x5229;&#x7528;[&#x7f51;&#x9875;&#x901f;&#x5ea6;&#x8bca;&#x65ad;&#x5de5;&#x5177;]">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="13"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+<!-- Copyright deathcult@163.com -->
+</map>
