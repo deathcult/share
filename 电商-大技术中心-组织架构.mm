01e10dd85883260da163176973d424e80df63fb7
deathcult <deathcult@163.com>
2013-11-10 14:45:14 +0800

    add new

---
+++ 电商-大技术中心-组织架构.mm
@@ -0,0 +1,81 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1340477643579" ID="ID_1418412825" MODIFIED="1384065622554" TEXT="&#x6280;&#x672f;&#x4e2d;&#x5fc3;">
+<font BOLD="true" NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="14"/>
+<node CREATED="1340477694374" ID="ID_1942111218" MODIFIED="1383927581589" POSITION="right" TEXT="&#x7f51;&#x7ad9;&#x7814;&#x53d1;&#x90e8;">
+<font BOLD="true" NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+<node CREATED="1340477847009" ID="ID_1818587017" MODIFIED="1383927279009" TEXT="&#x6d3b;&#x52a8;&#x3001;&#x4e13;&#x9898;">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1340477852761" ID="ID_958508824" MODIFIED="1383927284456" TEXT="&#x56e2;&#x8d2d;">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1340477858233" ID="ID_1453660845" MODIFIED="1383927291600" TEXT="&#x5546;&#x57ce;">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1383927761176" ID="ID_879535826" MODIFIED="1383927765828" TEXT="&#x540e;&#x53f0;&#x7ba1;&#x7406;"/>
+<node CREATED="1383927721241" ID="ID_725499403" MODIFIED="1383927744604" TEXT="&#x7b2c;&#x4e09;&#x65b9;&#x5e73;&#x53f0;"/>
+<node CREATED="1383927776760" ID="ID_1635917758" MODIFIED="1383927788340" TEXT="&#x79fb;&#x52a8;&#x5ba2;&#x6237;&#x7aef;&#x3001;APP"/>
+</node>
+<node CREATED="1383032834964" ID="ID_1793857863" MODIFIED="1383927036160" POSITION="left" TEXT="&#x8fd0;&#x7ef4;&#x90e8;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1340478177779" ID="ID_162233556" MODIFIED="1383927256304" TEXT="&#x786c;&#x4ef6;&#x3001;&#x7cfb;&#x7edf;&#x3001;&#x7f51;&#x7edc;">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1340478664008" ID="ID_1193779207" MODIFIED="1383927264425" TEXT="&#x5e73;&#x53f0;&#x8fd0;&#x7ef4;&#x3001;&#x7248;&#x672c;&#x53d1;&#x5e03;">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1340478479028" ID="ID_12544586" MODIFIED="1383927194378" TEXT="&#x6d4b;&#x8bd5;">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1383927562650" ID="ID_1942996812" MODIFIED="1383927567757" TEXT="&#x6280;&#x672f;&#x652f;&#x6301;"/>
+<node CREATED="1383927632218" ID="ID_118360455" MODIFIED="1383927639764" TEXT="&#x8d44;&#x4ea7;&#x7ba1;&#x7406;"/>
+<node CREATED="1383927653689" ID="ID_1111032842" MODIFIED="1383927679796" TEXT="&#x7f51;&#x7ad9;&#x57fa;&#x7840;&#x670d;&#x52a1;"/>
+</node>
+<node CREATED="1340477690310" ID="ID_405632583" MODIFIED="1383927111508" POSITION="left" TEXT="&#x5927;&#x6570;&#x636e;&#x90e8;">
+<font BOLD="true" NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+<node CREATED="1340477963639" ID="ID_1575379134" MODIFIED="1383927205769" TEXT="BI">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1383927825624" ID="ID_455610563" MODIFIED="1383927841292" TEXT="&#x6d41;&#x91cf;&#x7edf;&#x8ba1;&#x3001;&#x6570;&#x636e;&#x5206;&#x6790;"/>
+<node CREATED="1340478493315" ID="ID_472031470" MODIFIED="1383927210130" TEXT="DBA">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1340477683999" ID="ID_1692786210" MODIFIED="1383927121900" POSITION="left" TEXT="&#x8bbe;&#x8ba1;&#x90e8;">
+<font BOLD="true" NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+<node CREATED="1340477772473" ID="ID_1046895299" MODIFIED="1383927136428" TEXT="&#x524d;&#x7aef;">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1340477779281" ID="ID_1154718650" MODIFIED="1383927157707" TEXT="JS">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1340477755639" ID="ID_199150577" MODIFIED="1383927131051" TEXT="&#x8bbe;&#x8ba1;&#x5e08;">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1340477699100" ID="ID_1878616750" MODIFIED="1383927603613" POSITION="right" TEXT="&#x4e1a;&#x52a1;&#x7cfb;&#x7edf;&#x90e8;">
+<font BOLD="true" NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+<node CREATED="1340477970541" ID="ID_1219607853" MODIFIED="1383927328599" TEXT="ERP">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1340477986933" ID="ID_1238480396" MODIFIED="1383927336487" TEXT="WMS">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1340478007005" ID="ID_1496505207" MODIFIED="1383927342407" TEXT="CRM">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1340478852446" ID="ID_84263308" MODIFIED="1383927349583" TEXT="&#x5206;&#x9500;&#x7cfb;&#x7edf;">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+<node CREATED="1340478865040" ID="ID_556602720" MODIFIED="1383927360487" TEXT="&#x57fa;&#x7840;&#x63a5;&#x53e3;">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1340477865147" ID="ID_367118993" MODIFIED="1383927607957" POSITION="right" TEXT="IT&#x90e8;">
+<font BOLD="true" NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+<node CREATED="1383927614938" ID="ID_523536490" MODIFIED="1383927619784" TEXT="&#x529e;&#x516c;IT&#x8fd0;&#x7ef4;"/>
+<node CREATED="1383927621433" ID="ID_1822536279" MODIFIED="1383927626028" TEXT="&#x8d44;&#x4ea7;&#x7ba1;&#x7406;"/>
+</node>
+</node>
+</map>
