790519adf9848de05874e4d9834786ccf6acaa8a
deathcult <deathcult@163.com>
2013-12-20 12:04:58 +0800

    rename, update

---
+++ 大型网站-运维中心.mm
@@ -0,0 +1,376 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1340477643579" ID="ID_1418412825" MODIFIED="1387512088998" STYLE="bubble" TEXT="&#x5927;&#x578b;&#x7f51;&#x7ad9;-&#x8fd0;&#x7ef4;&#x4e2d;&#x5fc3;">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1342435254593" ID="ID_180183656" MODIFIED="1387512088998" POSITION="left" STYLE="bubble" TEXT="&#x786c;&#x4ef6;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1345880695281" ID="ID_1726212885" MODIFIED="1387512088998" STYLE="bubble" TEXT="&#x670d;&#x52a1;&#x5668;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1345863015500" ID="ID_1451982946" MODIFIED="1387512088998" STYLE="bubble" TEXT="HP1U*20">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1387511215138" ID="ID_1297275429" MODIFIED="1387512088998" STYLE="bubble" TEXT="Dell 1U*30">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1345863039968" ID="ID_864171709" MODIFIED="1387512088998" STYLE="bubble" TEXT="Dell 2U*12">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1387511222393" ID="ID_1161342079" MODIFIED="1387512088997" STYLE="bubble" TEXT="Solaris 5">
+<edge STYLE="bezier"/>
+</node>
+</node>
+<node CREATED="1345863044906" ID="ID_61052771" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x4ea4;&#x6362;&#x673a;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1381547814630" ID="ID_1379412643" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x8def;&#x7531;&#x5668;">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381547810567" ID="ID_929031667" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x5176;&#x4ed6;&#x8bbe;&#x5907;">
+<edge STYLE="bezier"/>
+<node CREATED="1381547896678" ID="ID_97681767" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x8d1f;&#x8f7d;&#x5747;&#x8861;">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381547904373" ID="ID_553415235" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x9632;&#x706b;&#x5899;">
+<edge STYLE="bezier"/>
+</node>
+</node>
+</node>
+<node CREATED="1345862939031" ID="ID_1748337306" MODIFIED="1387512088997" POSITION="left" STYLE="bubble" TEXT="&#x7f51;&#x7edc;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1345863433265" ID="ID_1558440566" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x67b6;&#x6784;&#x8bbe;&#x8ba1;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345863440890" ID="ID_1864178121" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x90e8;&#x7f72;&#x5b9e;&#x65bd;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345878450312" ID="ID_1987484222" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x5e26;&#x5bbd;&#x6d41;&#x91cf;IP&#x89c4;&#x5212;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1345862945109" ID="ID_82111099" MODIFIED="1387512088997" POSITION="left" STYLE="bubble" TEXT="&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1342414968951" ID="ID_1960620227" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x9009;&#x578b;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1342435045214" ID="ID_322111447" MODIFIED="1387512088997" STYLE="bubble" TEXT="Windows Server">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1342435052868" ID="ID_1735792805" MODIFIED="1387512088997" STYLE="bubble" TEXT="Linux">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1387511301896" ID="ID_1320327878" MODIFIED="1387512088997" STYLE="bubble" TEXT="CentOS">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1387511313465" ID="ID_662668079" MODIFIED="1387512088997" STYLE="bubble" TEXT="RH AS">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1387511306600" ID="ID_177581156" MODIFIED="1387512088997" STYLE="bubble" TEXT="Debian">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1387511331209" ID="ID_1845973804" MODIFIED="1387512088997" STYLE="bubble" TEXT="Ubuntu">
+<edge STYLE="bezier"/>
+</node>
+</node>
+<node CREATED="1381547989829" ID="ID_118822359" MODIFIED="1387512088997" STYLE="bubble" TEXT="Solaris">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381547998725" ID="ID_362028731" MODIFIED="1387512088997" STYLE="bubble" TEXT="Freebsd">
+<edge STYLE="bezier"/>
+</node>
+</node>
+<node CREATED="1387511368328" ID="ID_1915332608" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x7ba1;&#x7406;">
+<edge STYLE="bezier"/>
+<node CREATED="1345863412921" ID="ID_1114693879" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x5b89;&#x88c5;&#x914d;&#x7f6e;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345863381296" ID="ID_236866639" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x5347;&#x7ea7;&#x8865;&#x4e01;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345863418218" ID="ID_1685983361" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x5b89;&#x5168;&#x9632;&#x62a4;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1345880416718" ID="ID_170972572" MODIFIED="1387512088997" POSITION="left" STYLE="bubble" TEXT="&#x6545;&#x969c;&#x5904;&#x7406;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1345880434125" ID="ID_474098947" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x5907;&#x4efd;&#x7b56;&#x7565;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345880511593" ID="ID_800380848" MODIFIED="1387512088997" STYLE="bubble" TEXT="&#x786c;&#x4ef6;&#x5197;&#x4f59;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345880438640" ID="ID_979650684" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x6062;&#x590d;&#x673a;&#x5236;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345880443906" ID="ID_71966558" MODIFIED="1387512088996" STYLE="bubble" TEXT="7&#xd7;24&#x5c0f;&#x65f6;&#x54cd;&#x5e94;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1345862949750" ID="ID_138813905" MODIFIED="1387512088996" POSITION="right" STYLE="bubble" TEXT="&#x5e94;&#x7528;&#x670d;&#x52a1;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1345869294968" ID="ID_233867164" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x4e1a;&#x52a1;&#x5e94;&#x7528;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1387511481895" ID="ID_1956691179" MODIFIED="1387512088996" STYLE="bubble" TEXT="Varnish/Squid">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1387511517846" ID="ID_1989190167" MODIFIED="1387512088996" STYLE="bubble" TEXT="LVS/HAproxy/Keepalived">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1342434677618" ID="ID_241380663" MODIFIED="1387512088996" STYLE="bubble" TEXT="Apache/Nginx/Lighttpd">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1342434689532" ID="ID_1616123324" MODIFIED="1387512088996" STYLE="bubble" TEXT="Tomcat/Resin/Jboss">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1381548438162" ID="ID_3656690" MODIFIED="1387512088996" STYLE="bubble" TEXT="Memcache/Redis/Sphinx/TCTB">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381548432657" ID="ID_853784059" MODIFIED="1387512088996" STYLE="bubble" TEXT="MongoDB/CouchDB">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1387511643830" ID="ID_985145086" MODIFIED="1387512088996" STYLE="bubble" TEXT="Sendmail/Postfix">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1387511654694" ID="ID_376531964" MODIFIED="1387512088996" STYLE="bubble" TEXT="Bind/DNS">
+<edge STYLE="bezier"/>
+</node>
+</node>
+<node CREATED="1342434697096" ID="ID_950237560" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x5e93;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1387511749478" ID="ID_327413509" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x6700;&#x597d;&#x7531;DBA&#x8d1f;&#x8d23;">
+<edge STYLE="bezier"/>
+</node>
+</node>
+<node CREATED="1345869304593" ID="ID_15569509" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x7814;&#x53d1;&#x8f85;&#x52a9;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1345866519421" ID="ID_740736026" MODIFIED="1387512088996" STYLE="bubble" TEXT="SVN/CVS/GIT">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345866522500" ID="ID_1915195087" MODIFIED="1387512088996" STYLE="bubble" TEXT="VPN">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345866524718" ID="ID_449948626" MODIFIED="1387512088996" STYLE="bubble" TEXT="Bugzilla/Mantis/JIRA">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1381548489170" ID="ID_120671118" MODIFIED="1387512088996" STYLE="bubble" TEXT="Issue Tracking">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381548498081" ID="ID_1907368445" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x9879;&#x76ee;&#x7ba1;&#x7406;/&#x4ea7;&#x54c1;&#x7ba1;&#x7406;/&#x9700;&#x6c42;&#x7ba1;&#x7406;&#x7cfb;&#x7edf;">
+<edge STYLE="bezier"/>
+</node>
+</node>
+</node>
+<node CREATED="1345863239046" ID="ID_1219588043" MODIFIED="1387512088996" POSITION="right" STYLE="bubble" TEXT="&#x8d44;&#x4ea7;&#x7ba1;&#x7406;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1381548572608" ID="ID_353528078" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x8d44;&#x4ea7;">
+<edge STYLE="bezier"/>
+<node CREATED="1345863245234" ID="ID_1927979841" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x786c;&#x4ef6;&#x8d44;&#x4ea7;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345863252843" ID="ID_1466290957" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x8f6f;&#x4ef6;&#x8d44;&#x4ea7;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345863258687" ID="ID_1323095035" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x670d;&#x52a1;&#x5e10;&#x6237;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1345863563234" ID="ID_457787874" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x91c7;&#x8d2d;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345879183906" ID="ID_1683236117" MODIFIED="1387512088996" STYLE="bubble" TEXT="&#x8d39;&#x7528;&#x5ba1;&#x6838;/&#x7533;&#x8bf7;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345863568500" ID="ID_1855018439" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x9884;&#x7b97;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1345862954734" ID="ID_1323501124" MODIFIED="1387512088995" POSITION="right" STYLE="bubble" TEXT="&#x5176;&#x4ed6;&#x4e1a;&#x52a1;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1342414847654" ID="ID_1664517897" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x57df;&#x540d;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1342162429523" ID="ID_989626694" MODIFIED="1387512088995" STYLE="bubble" TEXT="DNS">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1381548835406" ID="ID_735599434" MODIFIED="1387512088995" STYLE="bubble" TEXT="dnspod">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1387511946884" ID="ID_1462443112" MODIFIED="1387512088995" STYLE="bubble" TEXT="DNSimple">
+<edge STYLE="bezier"/>
+</node>
+</node>
+<node CREATED="1342414893513" ID="ID_1343823703" MODIFIED="1387512088995" STYLE="bubble" TEXT="CDN">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1381548848958" ID="ID_1688188292" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x5e1d;&#x8054;">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381548851982" ID="ID_670850764" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x7f51;&#x5bbf;">
+<edge STYLE="bezier"/>
+</node>
+</node>
+<node CREATED="1381548730607" ID="ID_217449911" MODIFIED="1387512088995" STYLE="bubble" TEXT="VPS">
+<edge STYLE="bezier"/>
+<node CREATED="1381548857471" ID="ID_357948551" MODIFIED="1387512088995" STYLE="bubble" TEXT="Linode">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1387511843269" ID="ID_1982784680" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x5176;&#x4ed6;&#x4e91;">
+<edge STYLE="bezier"/>
+</node>
+</node>
+<node CREATED="1381548925822" ID="ID_1973930241" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x4f01;&#x4e1a;&#x90ae;&#x7bb1;">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1342162433197" ID="ID_1627161152" MODIFIED="1387512088995" STYLE="bubble" TEXT="IDC">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345878873859" ID="ID_36812027" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x76d1;&#x63a7;&#x5b9d;/&#x65e5;&#x5fd7;&#x5b9d;/360">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1381548797887" ID="ID_405033494" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x7edf;&#x8ba1;&#x5206;&#x6790;&#x670d;&#x52a1;">
+<edge STYLE="bezier"/>
+<node CREATED="1381548891855" ID="ID_1816535242" MODIFIED="1387512088995" STYLE="bubble" TEXT="GA">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381548867854" ID="ID_440021460" MODIFIED="1387512088995" STYLE="bubble" TEXT="baidu">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381548870606" ID="ID_156137855" MODIFIED="1387512088995" STYLE="bubble" TEXT="tencent">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381548873679" ID="ID_313321725" MODIFIED="1387512088995" STYLE="bubble" TEXT="cnzz">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381548879343" ID="ID_27455604" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x91cf;&#x5b50;&#x6052;&#x9053;">
+<edge STYLE="bezier"/>
+</node>
+</node>
+<node CREATED="1345827759562" ID="ID_625127813" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x7f51;&#x7ad9;&#x5907;&#x6848;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345827743859" ID="ID_1330551957" MODIFIED="1387512088995" STYLE="bubble" TEXT="ICP/BBS&#x8bc1;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345878901500" ID="ID_1170916581" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x89c6;&#x9891;&#x52a0;&#x901f;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345878904500" ID="ID_1349661138" MODIFIED="1387512088995" STYLE="bubble" TEXT="&#x77ed;&#x4fe1;/&#x5f69;&#x4fe1;&#x63a5;&#x53e3;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1345862963015" ID="ID_986030110" MODIFIED="1387512088995" POSITION="left" STYLE="bubble" TEXT="&#x5b89;&#x5168;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1342434866733" ID="ID_1883445769" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x5e10;&#x53f7;&#x5bc6;&#x7801;/&#x6743;&#x9650;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345866659843" ID="ID_1747304355" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x9632;&#x5165;&#x4fb5;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345866664531" ID="ID_674463044" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x9632;&#x653b;&#x51fb;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345878633765" ID="ID_181696440" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x6f0f;&#x6d1e;&#x4fee;&#x590d;/&#x8865;&#x4e01;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1342162448702" ID="ID_1642832123" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x76d1;&#x63a7;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1345827931875" ID="ID_376767048" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x6027;&#x80fd;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345827936453" ID="ID_1412905403" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x8d1f;&#x8f7d;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345827963125" ID="ID_1343068767" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x7a7a;&#x95f4;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1345827940656" ID="ID_1751896954" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x5b89;&#x5168;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1381548272275" ID="ID_854760977" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x62a5;&#x8b66;">
+<edge STYLE="bezier"/>
+</node>
+</node>
+<node CREATED="1345862959031" ID="ID_1349522787" MODIFIED="1387512088994" POSITION="left" STYLE="bubble" TEXT="&#x6570;&#x636e;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1345863126359" ID="ID_1859026975" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x5907;&#x4efd;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1381548072644" ID="ID_834299971" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x5b58;&#x50a8;">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381548067380" ID="ID_1901753322" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x4f20;&#x8f93;">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1345863118781" ID="ID_618988887" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x4e1a;&#x52a1;&#x9700;&#x6c42;&#x5904;&#x7406;">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1387511452024" ID="ID_327534157" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x8fc7;&#x6ee4;">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381548076916" ID="ID_929104298" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x6e05;&#x6d17;">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1381548097476" ID="ID_942484616" MODIFIED="1387512088994" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x8f6c;&#x6362;">
+<edge STYLE="bezier"/>
+</node>
+</node>
+</node>
+</node>
+</map>
